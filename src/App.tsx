2cffa70fbc56b070336417335f6caf8b0913113e
Rosilene Farias <rosilene.farias00@gmail.com>
2025-02-12 14:33:05 -0300

    Criando as Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
 import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Cadastro from './pages/cadastro/Cadastro'
+import Footer from './assets/components/footer/Footer.tsx/Footer'
+import Navbar from './assets/components/navbar/Navbar'
 
 function App() {
-    return (
-        <Home />
-    )
+  return (
+    <>
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
+  )
 }
 
 export default App
