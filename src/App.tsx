b9e889720d07dea305f0fa21a630d75ab1e2a4fc
Rosilene Farias <rosilene.farias00@gmail.com>
2025-02-13 14:27:20 -0300

    interface model e service

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,32 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Home from './pages/home/Home'
-import Login from './pages/login/Login'
-import Cadastro from './pages/cadastro/Cadastro'
-import Footer from './assets/components/footer/Footer.tsx/Footer'
-import Navbar from './assets/components/navbar/Navbar'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Cadastro from "./pages/cadastro/Cadastro";
+
+import Footer from "./assets/components/footer/Footer.tsx/Footer";
+import Navbar from "./assets/components/navbar/Navbar";
+
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
-    <>
-        <BrowserRouter>
-          <Navbar />
-          <div className="min-h-[80vh]">
-            <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/cadastro" element={<Cadastro />} />
-              <Route path="/login" element={<Login />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
-    </>
-  )
+    <AuthProvider>
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
